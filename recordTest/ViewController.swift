28556bcb143837fc562288f5feba48de8315f10f
ginnanikasuos <e.onzuka@gmail.com>
2018-09-24 08:49:12 +0900

    Initial Commit

---
+++ recordTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recordTest
+//
+//  Created by 隠塚永治 on 2018/09/23.
+//  Copyright © 2018年 隠塚永治. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
